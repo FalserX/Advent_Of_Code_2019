d7e8bf5343ee02c6cd90491b814d3d62baccdc2c
FalserX <w.malenfant@outlook.com>
2020-08-06 10:28:50 -0400

    Add files via upload

---
+++ AOC2019/AdventCode2019_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace AdventCode2019_1
+{
+    class Program
+    {
+        string[] strData; //The array of data from a text file.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        void populateArrayOfData()
+        {
+
+        }
+        
+
+    }
+}
