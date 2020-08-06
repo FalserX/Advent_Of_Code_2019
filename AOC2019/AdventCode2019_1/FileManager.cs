7bb25a26cc464c916debcf823b6de661cec4c2f6
William Malenfant <w.malenfant@outlook.com>
2020-08-06 13:32:41 -0400

    Adjustment of the variables

--- AOC2019/AdventCode2019_1/FileManager.cs
+++ AOC2019/AdventCode2019_1/FileManager.cs
@@ -12,25 +12,25 @@ namespace AdventCode2019_1
         /// <summary>
         /// Method to get the root of the application
         /// </summary>
-        /// <param name="strFileName">The name of the application</param>
+        /// <param name="p_strFileName">The name of the application</param>
         /// <returns>The path to the root</returns>
-        public string GetAppRoot(string strFileName)
+        public string GetAppRoot(string p_strFileName)
         {
-            var execPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
-            Regex appRootMatch = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
-            var appRoot = appRootMatch.Match(execPath).Value;
-            return Path.Combine(appRoot, strFileName);
+            var m_execPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Regex m_appRootMatch = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var m_appRoot = m_appRootMatch.Match(m_execPath).Value;
+            return Path.Combine(m_appRoot, p_strFileName);
         }
 
         /// <summary>
         /// Method to populate the array of data string from a text file or another type of file.
         /// </summary>
-        /// <param name="strPathOfDataFile">The path of the data file</param>\
+        /// <param name="p_strPathOfDataFile">The path of the data file</param>\
         /// <returns>The string of data in array form</returns>
-        public string[] PopulateArrayOfData(string strPathOfDataFile)
+        public string[] PopulateArrayOfData(string p_strPathOfDataFile)
         {
-            string strData = File.ReadAllText(strPathOfDataFile);
-            return strData.Split("\r\n",StringSplitOptions.RemoveEmptyEntries);
+            string m_strData = File.ReadAllText(p_strPathOfDataFile);
+            return m_strData.Split("\r\n",StringSplitOptions.RemoveEmptyEntries);
         }
 
 
--- AOC2019/AdventCode2019_1/Program.cs
+++ AOC2019/AdventCode2019_1/Program.cs
@@ -6,16 +6,17 @@ namespace AdventCode2019_1
     {
         static string[] strDataSplit; //The array of data from a text file.
         static int[] iDataSplitConvert; //The array of data convert from strDataSplit.
+        static int[] iDataRequired; //The array of data from which what is required and not the total.
         /// <summary>
         /// Method master from the program
         /// </summary>
         /// <param name="args">Arguments to launch</param>
         static void Main(string[] args)
         {
-            FileManager fm = new FileManager();
-            string strGetRoot = fm.GetAppRoot("Program.cs");
-            strGetRoot = strGetRoot.Split("Program.cs")[0];
-            strDataSplit = fm.PopulateArrayOfData(strGetRoot + @"\input.txt");
+            FileManager m_fm = new FileManager();
+            string m_strGetRoot = m_fm.GetAppRoot("Program.cs");
+            m_strGetRoot = m_strGetRoot.Split("Program.cs")[0];
+            strDataSplit = m_fm.PopulateArrayOfData(m_strGetRoot + @"\input1_2.txt");
             iDataSplitConvert = new int[strDataSplit.Length];
             try
             {
@@ -23,27 +24,37 @@ namespace AdventCode2019_1
                 {
                     iDataSplitConvert[i] = int.Parse(strDataSplit[i]);
                 }
+
                 Console.WriteLine(SumOfData(iDataSplitConvert));
             }
-            catch(Exception e)
+            catch(Exception m_e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(m_e.Message);
             }
         }
         /// <summary>
         /// Method to get the sum of all of data get from a text file
         /// </summary>
-        /// <param name="iData">The array of data in int format</param>
+        /// <param name="p_iData">The array of data in int format</param>
         /// <returns>The sum of the data</returns>
-        static int SumOfData(int[] iData)
+        static int SumOfData(int[] p_iData)
         {
-            int sum = 0;
-            for(int i = 0; i < iData.Length;i++)
+            int m_sum = 0;
+            for(int i = 0; i < p_iData.Length;i++)
             {
-                sum += (iData[i] / 3) - 2;
+                m_sum += (p_iData[i] / 3) - 2;
             }
-            return sum;
+            return m_sum;
         }
         
+        static int RecurSumOfData(int[] p_iData)
+        {
+            return 0;
+        }
+
+        static private int _RecurSumOfData(int p_d)
+        {
+            return 0;
+        }
     }
 }
