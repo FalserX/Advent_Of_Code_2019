4f510659357cc9a90eacef69307ebf1d9488f0bc
William Malenfant <w.malenfant@outlook.com>
2020-08-06 23:50:15 -0400

    Finishing the code of part 1 from day 2 of Advent Of Code 2019

---
+++ AOC2019/AdventCode2019_2/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventCode2019_2
+{
+    public class FileManager
+    {
+
+        public FileManager() { }
+
+        /// <summary>
+        /// Method to get the root of the application
+        /// </summary>
+        /// <param name="p_strFileName">The name of the application</param>
+        /// <returns>The path to the root</returns>
+        public string GetAppRoot(string p_strFileName)
+        {
+            var m_execPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Regex m_appRootMatch = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var m_appRoot = m_appRootMatch.Match(m_execPath).Value;
+            return Path.Combine(m_appRoot, p_strFileName);
+        }
+
+        /// <summary>
+        /// Method to populate the array of data string from a text file or another type of file.
+        /// </summary>
+        /// <param name="p_strPathOfDataFile">The path of the data file</param>\
+        /// <returns>The string of data in array form</returns>
+        public string[] PopulateArrayOfData(string p_strPathOfDataFile)
+        {
+            string m_strData = File.ReadAllText(p_strPathOfDataFile);
+            return m_strData.Split("\r\n",StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Method to writing, to the file, the data receive
+        /// </summary>
+        /// <param name="p_strPathOfDataFile">The path of the data file</param>
+        /// <param name="p_strData">The data</param>
+        public void WriteData(string p_strPathOfDataFile,string p_strData)
+        {
+            File.WriteAllText(p_strPathOfDataFile,p_strData);
+        }
+
+    }
+}
--- AOC2019/AdventCode2019_2/Program.cs
+++ AOC2019/AdventCode2019_2/Program.cs
@@ -4,8 +4,60 @@ namespace AdventCode2019_2
 {
     class Program
     {
+        static string[] m_data; //Contains the data split into array of data without blank space from each line delimiter.
+        static string[] m_dataSplit; //Contains the data split into array without commas.
+        static int[] m_dataConvert; //Contains the same data than m_dataSplit, but in integer format.
+        /// <summary>
+        /// Method master from the program
+        /// </summary>
+        /// <param name="args">Arguments to launch</param>
         static void Main(string[] args)
         {
+            FileManager m_fmDataFile = new FileManager();
+            m_data = m_fmDataFile.PopulateArrayOfData((m_fmDataFile.GetAppRoot("Program.cs").Split("Program.cs")[0]) + @"\input.txt");
+            m_dataSplit = m_data[0].Split(',');
+            m_dataConvert = new int[m_dataSplit.Length];
+            try
+            {
+                for (int i = 0; i < m_dataConvert.Length; i++)
+                {
+                    m_dataConvert[i] = int.Parse(m_dataSplit[i]);
+                }
+
+                for(int i = 0; i < m_dataConvert.Length;i+=4)
+                {
+                    int posFinal = m_dataConvert[3 + i];
+                    if (m_dataConvert[i] != 99)
+                    {
+                        if (m_dataConvert[i] == 1) //For addition
+                        {
+                            m_dataConvert[posFinal] = m_dataConvert[m_dataConvert[1 + i]] + m_dataConvert[m_dataConvert[2 + i]];
+                        }
+                        else if (m_dataConvert[i] == 2) //For multiplication
+                        {
+                            m_dataConvert[posFinal] = m_dataConvert[m_dataConvert[1 + i]] * m_dataConvert[m_dataConvert[2 + i]];
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                string m_dataReceive = "";
+                for(int i = 0; i < m_dataConvert.Length;i++)
+                {
+                    m_dataReceive += m_dataConvert[i].ToString() + ",";
+                }
+                m_fmDataFile.WriteData((m_fmDataFile.GetAppRoot("Program.cs").Split("Program.cs")[0]) + @"\inputResult.txt", 
+                                        m_dataReceive.Substring(0, m_dataReceive.Length - 1));
+
+                
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
